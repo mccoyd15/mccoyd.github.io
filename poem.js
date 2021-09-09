97e6170990bed5b6c6fe4494792ba59b7dd55666
Destiney <mccoyd@berea.edu>
2021-09-09 02:49:07 -0400

    changes to webpage

--- poem.js
+++ poem.js
@@ -1,10 +1,8 @@
 function nextLine(line) {
 
-	var poem = new Array("<p onClick=nextLine(1)>There once was a maintainer on github</p>",
-"<p onClick=nextLine(2)>So many pull requests she did snub</p>",
-"<p onClick=nextLine(3)>So I forked the repo</p>",
-"<p onClick=nextLine(4)>Now they all send <em>me</em> changes, d'oh!</p>",
-"<p onClick=nextLine(0)>Said the maintainer, 'Welcome to the club!'</p>");
+	var poem = new Array("<p onClick=nextLine(1)>Dean’s List</p>",
+"<p onClick=nextLine(2)>Berea College Free Tuition Scholarship </p>",
+"<p onClick=nextLine(3)>Blue Cross Blue Shield scholarship</p>");
 
 	document.getElementById("line").innerHTML=poem[line];
 }
